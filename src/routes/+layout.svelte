950b12bcfad59f7c664e48aa2e889fa3e3c7c211
EHMD28 <ehmdiagne0530@gmail.com>
2025-03-26 13:45:31 -0400

    might as well

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,18 +1,19 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+	import { base } from '$app/paths';
 </script>
 
 <nav
 	class="flex h-20 w-screen items-center justify-between bg-blue-800 px-10 text-2xl text-gray-100"
 >
-	<h1><a href="/">EHMD28's Personal Website</a></h1>
+	<h1><a href="{base}/">EHMD28's Personal Website</a></h1>
 	<div class="space-between flex w-1/2 justify-between text-lg">
-		<h2><a href="/projects/c">C Projects</a></h2>
-		<h2><a href="/projects/java">Java Projects</a></h2>
-		<h2><a href="/projects/python">Python Projects</a></h2>
-		<h2><a href="/projects/rust">Rust Projects</a></h2>
-		<h2><a href="/projects/electronics">Electronics Projects</a></h2>
+		<h2><a href="{base}/projects/c">C Projects</a></h2>
+		<h2><a href="{base}/projects/java">Java Projects</a></h2>
+		<h2><a href="{base}/projects/python">Python Projects</a></h2>
+		<h2><a href="{base}/projects/rust">Rust Projects</a></h2>
+		<h2><a href="{base}/projects/electronics">Electronics Projects</a></h2>
 	</div>
 </nav>
 
