ed00984f21ab5bc440c7f9ee3b8a6b89a36d3961
EHMD <112766808+EHMD28@users.noreply.github.com>
2025-08-25 09:40:17 -0400

    Update svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -6,7 +6,7 @@ const config = {
 			fallback: '404.html'
 		}),
 		paths: {
-			base: process.argv.includes('dev') ? '' : '/ehmd28.github.io'
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
 	}
 };
