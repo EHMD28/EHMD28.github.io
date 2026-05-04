cac43eb653b895fcaab41d165dbd99e5eb1d9a1d
EHMD28 <ehmdiagne0530@gmail.com>
2026-05-04 11:03:02 -0400

    started implementing layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import { resolve } from "$app/paths";
+	import favicon from "$lib/assets/favicon.svg";
+	import "../app.css";
+	import "../themes/dark.css";
 
 	let { children } = $props();
 </script>
@@ -8,4 +11,22 @@
 	<link rel="icon" href={favicon} />
 </svelte:head>
 
-{@render children()}
+<nav class="dark">
+	<h1>EHMD28 Personal Website</h1>
+	<div>
+		<a href={resolve("/")}>Home</a>
+		<a href={resolve("/about")}>Projects</a>
+		<a href={resolve("/programs")}>Programs</a>
+		<a href={resolve("/projects")}>About</a>
+	</div>
+</nav>
+<main>
+	{@render children()}
+</main>
+
+<style>
+	nav {
+		background-color: var(--md-sys-color-primary-container);
+		color: var(--md-sys-color-on-primary-container);
+	}
+</style>
