93b864234f1f3689c051caa5f379469467f200c2
EHMD28 <ehmdiagne0530@gmail.com>
2026-05-07 17:35:43 -0400

    added email to footer

--- src/lib/components/main/Footer.svelte
+++ src/lib/components/main/Footer.svelte
@@ -1,8 +1,13 @@
 <script>
+    import emailLogo from "$lib/assets/main/logos/envelope-regular-full.svg";
     import githubLogo from "$lib/assets/main/logos/github-brands-solid.svg";
 </script>
 
 <footer class="dark">
+    <a href="mailto:ehmdiagne0530@gmail.com" target="_blank">
+        <img src={emailLogo} width={48} alt="" />
+        <span>Email: ehmdiagne0530@gmail.com</span>
+    </a>
     <a href="https://github.com/EHMD28" target="_blank">
         <img src={githubLogo} width={48} alt="" />
         <span>Github</span>
@@ -11,7 +16,9 @@
 
 <style>
     footer {
-        flex: 0 1 auto;
+        display: flex;
+        flex-direction: row;
+        gap: 50px;
 
         padding: 2vh 2vw;
 
