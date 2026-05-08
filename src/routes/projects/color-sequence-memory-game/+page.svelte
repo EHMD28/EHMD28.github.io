02af0a19f3267a3b72ebea8ce0b8612fb5b67db1
EHMD28 <ehmdiagne0530@gmail.com>
2026-05-08 19:40:34 -0400

    added color sequence memory game article

---
+++ src/routes/projects/color-sequence-memory-game/+page.svelte
@@ -0,0 +1,35 @@
+<article>
+    <h1>Color Sequence Memory Game</h1>
+    <p>
+        This project was inspired by the <a
+            href="https://en.wikipedia.org/wiki/Simon_(game)"
+            target="_blank">Simon</a
+        >
+        memory game. I made it as part of a high school class project using an Arduino,
+        basic electrical components, and a seven-segment display. I also wrote an
+        <a href="https://www.instructables.com/Color-Sequence-Memory-Game/"
+            >Instructables</a
+        > article with instructions for how to recreate the game. This is a recreation
+        of my first ever Arduino project, but I wouldn't really say I learned anything
+        new except how to use a binary decoder to drive a 7-segment display.
+    </p>
+    <br />
+    <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/n0sbcxyNJTo?si=ZHu3qy3pX3UbDdsI"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+    ></iframe>
+</article>
+
+<style>
+    article {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
