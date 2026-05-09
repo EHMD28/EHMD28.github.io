2a7a3eab73318bc012548b6ab2619c90e4403b26
EHMD28 <ehmdiagne0530@gmail.com>
2026-05-08 22:18:00 -0400

    added lang game article

---
+++ src/routes/projects/lang-game/+page.svelte
@@ -0,0 +1,31 @@
+<article>
+    <h1>Programming Language Game</h1>
+    <p>
+        I made an <a href="https://github.com/EHMD28/SummerProject2025"
+            >online board game</a
+        > for an independent summer research project I had to do for school. The
+        project was open-ended, so I decided to research the history of programming
+        languages. I then made a board game where the objective is to match tiles
+        to spaces using hint cards and a relationship diagram between the different
+        spaces.
+    </p>
+    <br />
+    <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/URuhZ4BOC-M?si=UxeXIqpnbpJS5YZc"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin"
+        allowfullscreen
+    ></iframe>
+</article>
+
+<style>
+    article {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
